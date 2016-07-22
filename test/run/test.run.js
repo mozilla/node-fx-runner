6a3b65e938e678341350d92afa1816614644eb0c
asamuzaK (Kazz) <asamuzaK@users.noreply.github.com>
2016-07-22 13:29:48 +0900

    Fix --binary-args patch v2 (#4)
    * accept binary arguments input to be string or array (for compatibility)
    * add test

--- test/run/test.run.js
+++ test/run/test.run.js
@@ -13,6 +13,7 @@ var exec = utils.exec;
 var isWindows = /^win/.test(process.platform);
 var normalizeBinary = require("../../lib/utils").normalizeBinary;
 var cp = require("child_process");
+var concatBinArgs = require("../../lib/run").concatBinArgs;
 
 var fakeBinary = path.join(__dirname, "..", "utils", "dummybinary" +
   (isWindows ? ".bat" : ".sh"));
@@ -113,5 +114,25 @@ describe("fx-runner start", function () {
         done();
       });
     });
+  });
+});
+
+describe("concat binary arguments", function () {
+  it("binary arguments as string", function (done) {
+    var arr = concatBinArgs([], "-a b -c \"d e\"");
+    expect(arr[0]).to.be.equal("-a");
+    expect(arr[1]).to.be.equal("b");
+    expect(arr[2]).to.be.equal("-c");
+    expect(arr[3]).to.be.equal("\"d e\"");
+    done();
+  });
+
+  it("binary arguments as array", function (done) {
+    var arr = concatBinArgs([], ["-a", "b", "-c", "\"d e\""]);
+    expect(arr[0]).to.be.equal("-a");
+    expect(arr[1]).to.be.equal("b");
+    expect(arr[2]).to.be.equal("-c");
+    expect(arr[3]).to.be.equal("\"d e\"");
+    done();
   });
 });
