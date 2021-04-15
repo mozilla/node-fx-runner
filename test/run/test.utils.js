d53b2e2773aeb27daa4855f8980f6f87bdea3947
Sam McCreery <4602020+mccreery@users.noreply.github.com>
2021-04-15 16:16:41 +0100

    Clarify test comments for modified case
    Also removes an incorrect comment from the new case

--- test/run/test.utils.js
+++ test/run/test.utils.js
@@ -38,7 +38,9 @@ describe("lib/utils", function () {
     var expected = "fake\\binary\\path";
 
     // see ./mock-winreg.js
-    // Only mock keys in HKLM (local machine) hive
+    // Only mock keys in HKLM (local machine) hive.
+    // This test case checks the basic functionality if the user does not have a newer version of
+    // Firefox installed meaning the HKCU keys are not present.
     var winreg = function(options) {
       this.get = function(_, fn) {
         if (options.hive === winreg.HKLM) {
@@ -73,12 +75,13 @@ describe("lib/utils", function () {
       return;
     }
 
-    // Provide different paths depending on hive
+    // Provide different paths depending on hive.
+    // This test case checks that in order to find recent versions of Firefox, HKCU is tried
+    // before falling back on HKCU as in the test above.
     var expected = "fake\\binary\\path";
     var oldPath = "fake\\old\\binary\\path";
 
     // see ./mock-winreg.js
-    // Only mock keys in HKLM (local machine) hive
     var winreg = function(options) {
       this.get = function(_, fn) {
         if (options.hive === winreg.HKCU) {
